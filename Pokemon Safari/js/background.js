14b390075bf308cea3f63bc21dae274e451e2ecf
Escheria <butterflies013@gmail.com>
2014-05-12 23:00:46 +0200

    CSS changes to menu, battle
    + Another row in menu buttons
    + Split battle style from page-
    - broke Pokemon battle layout, need to fix it

--- Pokemon Safari/js/background.js
+++ Pokemon Safari/js/background.js
@@ -4,7 +4,6 @@ function genericOnClick(info, tab) {
     chrome.tabs.sendMessage(tabs[0].id, {}, 
     function(response){
         txt = response.data.replace(/\n/g, "|").replace(/\//g,"&");
-        //chrome.tabs.create({url : "http://cfreeley.pythonanywhere.com/" + txt}); 
         chrome.downloads.download({url : "http://cfreeley.pythonanywhere.com/" + txt, filename: "data.csv"});
     });
   });
